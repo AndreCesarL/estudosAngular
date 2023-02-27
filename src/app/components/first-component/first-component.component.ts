e2cfd7afb48575410f1166e4c172eebca9c51b69
AndreCesarL <andrec.lemos@hotmail.com>
2023-02-27 16:47:32 -0300

    first Commit

---
+++ src/app/components/first-component/first-component.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-first-component',
+  templateUrl: './first-component.component.html',
+  styleUrls: ['./first-component.component.css']
+})
+export class FirstComponentComponent {
+
+  name = "André";
+  age = 32;
+  profesion = "Programador";
+
+}
